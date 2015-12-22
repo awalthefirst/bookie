50d12bcd4e0e5cbc18f7ef3544e9b250107dc5b0
Abdul   Awali <abdulrazakawal@gmail.com>
2015-12-22 17:05:36 +0000

    dashboard route init

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET Dashboard Page. */
 router.get('/', function(req, res, next) {
+  if(!req.Authen){
+    return res.redirect('/');
+  }
   res.render('dashboard',{
       title:'dashboard'
   });
