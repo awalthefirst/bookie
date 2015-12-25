3a4246a19454a870f364b44969d24cc4f4a2a02c
Abdul   Awali <abdulrazakawal@gmail.com>
2015-12-25 02:55:44 +0000

    implement info update

--- routes/settings.js
+++ routes/settings.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var bookDb = require("../model/users")
 
 /* GET users listing. */
 
-router.get('/', function(req, res, next) {
- res.render('settings',{
-     title:'settings'
- });
+router.get('/', function (req, res, next) {
+  res.render('settings', {
+    title: 'settings'
+  });
+});
+
+router.post('/', function (req, res, next) {
+
+  bookDb.updateUserInfo({
+    username: req.user.username,
+    email: req.user.email,
+    phone: req.body.phone,
+    city: req.body.city,
+    reqion: req.body.reqion
+  }, function (err, data) {
+
+    if (err) {
+      res.render('settings', {
+        title: 'error'
+      });
+    }else{
+      res.redirect('/dashboard');
+    }
+
+  });
+
 });
 
 module.exports = router;
