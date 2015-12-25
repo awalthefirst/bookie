accb0109b28a7ac44ca9e04dc4aa6c72124ca8ad
Abdul   Awali <abdulrazakawal@gmail.com>
2015-12-25 02:54:00 +0000

    avial books are loaded to index and dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,14 +1,84 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
+var bookDb = require("../model/books")
+  /* GET Dashboard Page. */
 
-/* GET Dashboard Page. */
-router.get('/', function(req, res, next) {
-  if(!req.Authen){
+router.get('/', function (req, res, next) {
+  if (!req.Authen) {
     return res.redirect('/');
   }
-  res.render('dashboard',{
-      title:'dashboard'
+
+  bookDb.findAllBook({
+    username: req.user.username, 
+  }, function (err, data) {
+      res.render('dashboard', {
+        title: 'dashboard',
+        auth: req.Authen,
+        data: data
+      });
+      
+      
+    })
+});
+
+
+router.post('/', function (req, res, next) {
+  if (!req.Authen) {
+    return res.redirect('/');
+  }
+
+  var query = req.body.book.replace(/ /g, '_');
+  request("http://isbndb.com/api/v2/json/INDTFIJN/book/" + query, function (error, response, body) {
+    body = JSON.parse(body);
+
+    if (!error && response.statusCode == 200 &&
+      body.error == undefined) {
+
+      var book = body.data[0].title.toLowerCase();
+      var bookisbn = body.data[0].isbn10;
+      bookDb.findBook({
+        bookname: book,
+        username: req.user.username
+      }, function (err, data) {
+
+        if (err || data !== null) {
+          return respond('err');
+        };
+
+        bookDb.addBook({
+          bookname: book,
+          bookisbn: bookisbn,
+          username: req.user.username,
+          email: req.user.email,
+          status: false
+        }, function (err, data) {
+          if (err) {
+            console.log(err)
+            return respond('err');
+          }
+          
+         res.redirect('/dashboard');
+        });
+
+      });
+
+
+    }
+    else {
+      respond('err');
+    }
+
+
+    function respond(title) {
+      res.render('dashboard', {
+        title: title,
+        data: []
+      });
+    }
+
   });
+
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var userDb = require("../model/users");
+var bookDb = require("../model/books");
 var bcrypt = require('bcryptjs');
 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Bookie',
-    auth: req.Authen
-  });
+  if (req.Authen) {
+    bookDb.findAllBook({}, function (err, data) {
+
+      res.render('index', {
+        title: 'Bookie',
+        auth: req.Authen,
+        authUsername:req.user.username,
+        data: data
+      });
+      
+      
+    })
+  }
+  else {
+    res.render('index', {
+      title: 'Bookie',
+      auth: req.Authen
+    });
+
+  }
 });
 
 
@@ -20,20 +37,21 @@ router.post('/login', function (req, res, next) {
   userDb.findUser({
     email: req.body.email.toLowerCase()
   }, function (err, data) {
-   
-   //compare password with hash password
-    var userPass = (function(){
-      if(data === null){
+
+    //compare password with hash password
+    var userPass = (function () {
+      if (data === null) {
         return false;
-      }else{
-        return bcrypt.compareSync(req.body.password,data.password);
+      }
+      else {
+        return bcrypt.compareSync(req.body.password, data.password);
       }
     })();
-    
+
     //render home page with error
     if (err || data === null || !userPass) {
       req.BookieSession.reset();
-    res.render('index', {
+      res.render('index', {
         title: 'Login Error',
         auth: req.Authen,
         error: ''
@@ -43,8 +61,8 @@ router.post('/login', function (req, res, next) {
 
       //redirect to dashboard with user cookie/session
       var user = {
-        username:data.username,
-        email:data.email
+        username: data.username,
+        email: data.email
       };
       req.BookieSession.user = user;
       res.redirect('/dashboard');
@@ -59,7 +77,7 @@ router.post('/signup', function (req, res, next) {
 
   //hash user pass
   var hashPass = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-  
+
   //save user data 
   userDb.addUser({
     username: req.body.username.toLowerCase(),
@@ -84,8 +102,8 @@ router.post('/signup', function (req, res, next) {
     else {
       //redirect to dashboard with user cookie/session
       var user = {
-        username:data.username,
-        email:data.email
+        username: data.username,
+        email: data.email
       };
       req.BookieSession.user = user;
       res.redirect('/dashboard');
